dc8869e62e4bcf81812cb437b8319eac70d13355
Mohammad Moridi <m.moridi.2009@gmail.com>
2021-04-20 17:35:28 +0430

    Complete sample database hands-on

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,19 @@ import { BookModule } from './book/book.module';
 import { GenreModule } from './genre/genre.module';
 import { UserModule } from './user/user.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import UserEntity from '../db/user.entity';
+import BookEntity from '../db/book.entity';
+import GenreEntity from '../db/genre.entity';
+
 @Module({
-  imports: [HelloModule, BookModule, GenreModule, UserModule],
+  imports: [HelloModule, UserModule , BookModule, GenreModule,
+  TypeOrmModule.forFeature(
+    [UserEntity, BookEntity , GenreEntity],
+    ),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
