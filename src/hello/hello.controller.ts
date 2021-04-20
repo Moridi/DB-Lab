99b627de890eeff77a6d0ecd23bed2eb3d08e2df
Mohammad Moridi <m.moridi.2009@gmail.com>
2021-04-20 12:21:36 +0430

    Initialize the project

---
+++ src/hello/hello.controller.ts
@@ -0,0 +1,39 @@
+import { HelloService } from './hello.service'
+import { Controller, Post, Body, Get, Header, Query } from '@nestjs/common';
+import { PersonDto } from './dto/person.dto'
+import { ApiResponse, ApiBearerAuth, ApiQuery} from '@nestjs/swagger';
+
+@Controller('hello')
+export class HelloController {
+    constructor(
+        private readonly helloService: HelloService,
+    ) {}
+
+    @Header('Content-Type', 'application/json')
+    @ApiResponse({ status: 200, description: 'Say Hello!!!'})
+    @ApiResponse({ status: 400, description: 'Bad Request'})
+    @Post('welcome')
+    async sayWelcome(@Body() personDto: PersonDto): Promise<{data : String}> {
+        let msg = await this.helloService.welcome(personDto);
+        return {data : msg};
+    }
+
+    @ApiResponse({ status: 200})
+    @ApiQuery({
+        name: 'name',
+        required: true,
+        type: String
+    })
+    @ApiQuery({
+        name: 'year',
+        required: false,
+        type: Number,
+        description: "You can ignore it."
+    })
+    @Get('welcome')
+    async sayWelcome2(@Query('name') iName, @Query('year') iYear):
+            Promise<{data : String}> {
+        let msg = await this.helloService.welcome({name: iName, year: iYear});
+        return {data : msg};
+    }
+}
