24fa301a40f14144996bb5de09f902f1e54aeb9d
Mohammad Moridi <m.moridi.2009@gmail.com>
2021-04-23 19:00:16 +0430

    Complete CRUD for genre

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,16 +1,28 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Delete, Put, Param } from '@nestjs/common';
 import GenreService from './genre.service';
 import CreateGenreDto from './dto/create-genre.dto';
 
 @Controller('genre')
 export default class GenreController {
   constructor(private readonly genreServices: GenreService) {}
+  
   @Post()
   postGenre( @Body() genre: CreateGenreDto) {
     return this.genreServices.insert(genre);
   }
+
   @Get()
   getAll() {
     return this.genreServices.getAllGenre();
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.genreServices.remove(id)
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() genre: CreateGenreDto) {
+    return this.genreServices.update(id, genre)
   }
 }
