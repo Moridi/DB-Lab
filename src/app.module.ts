953e74c7ab3fda98eadaffe9cfa07a2a8604e2fc
Mohammad Moridi <m.moridi.2009@gmail.com>
2021-04-23 18:24:03 +0430

    Remove hello module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './hello/hello.module';
 import { BookModule } from './book/book.module';
 import { GenreModule } from './genre/genre.module';
 import { UserModule } from './user/user.module';
@@ -13,7 +12,7 @@ import BookEntity from '../db/book.entity';
 import GenreEntity from '../db/genre.entity';
 
 @Module({
-  imports: [HelloModule, UserModule , BookModule, GenreModule,
+  imports: [UserModule , BookModule, GenreModule,
   TypeOrmModule.forFeature(
     [UserEntity, BookEntity , GenreEntity],
     ),
